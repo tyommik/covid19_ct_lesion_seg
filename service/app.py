18f1c882bca5f0d95e3df41d849b84d408c0566b
tyommik <2fasti.miem@gmail.com>
2021-02-27 19:52:51 +0300

    Add service with inference

---
+++ service/app.py
@@ -0,0 +1,72 @@
+import imghdr
+from flask_dropzone import Dropzone
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort, \
+    send_from_directory, current_app, jsonify
+from werkzeug.utils import secure_filename
+from processing import process_file
+
+dropzone = Dropzone()
+app = Flask(__name__)
+dropzone.init_app(app)
+app.config['MAX_CONTENT_LENGTH'] = 1000 * 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.nii', '.zip']
+app.config['UPLOAD_PATH'] = 'uploads'
+app.config['DOWNLOAD_FOLDER'] = 'downloads'
+app.config['DROPZONE_MAX_FILES'] = 1
+app.config['DROPZONE_MAX_FILE_SIZE'] = 1000
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.zip, .nii, .nii.gz'
+app.config['DROPZONE_REDIRECT_VIEW'] = 'viewer'
+
+db = {}
+
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
+
+
+@app.route('/')
+def index():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('index.html', files=files)
+
+
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files.get('file')
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            return "Invalid image", 400
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        db['file'] = os.path.join(app.config['UPLOAD_PATH'], filename)
+    return "", 200
+
+
+@app.route('/viewer')
+def viewer():
+    return render_template('viewer.html')
+
+
+@app.route('/process')
+def process():
+    file = db.get('file')
+    if file is None:
+        return jsonify({"status": "err"}, 404)
+    result = process_file(file_path=file)
+    if result:
+        return jsonify({"status": "ok", "result": result}, 200)
+    return {"status": "err"}, 500
+
+
+@app.route('/downloads/<path:filename>', methods=['GET'])
+def download(filename):
+    downloads = os.path.join(current_app.root_path, app.config['DOWNLOAD_FOLDER'])
+    return send_from_directory(directory=downloads, filename=filename)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ service/processing.py
@@ -0,0 +1,95 @@
+import shutil
+import subprocess
+import zipfile
+import pathlib
+import uuid
+import torch
+from lungmask import mask as lm
+import SimpleITK as sitk
+import nibabel as nib
+import os
+import json
+
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+lungmask = lm.get_model('unet', 'R231CovidWeb')
+
+
+def get_inference(file_path, out_mask_file):
+    slice_lung_volume = 0
+    # Get lungmask inference from nii file
+    input_image = sitk.ReadImage(str(file_path))
+
+    # Calculate lung volume
+    segmentation = lm.apply(input_image, lungmask)
+
+    input_img = nib.load(str(file_path))
+    new_img = input_img.__class__(segmentation.T, input_img.affine, input_img.header)
+    new_img.to_filename(str(out_mask_file))
+    return segmentation
+
+
+def unzip(file_path):
+    output_dir = file_path.parent / file_path.stem
+    output_dir.mkdir(exist_ok=True)
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)
+
+    return output_dir
+
+
+def dcm2niix(file_path, fout):
+    p = subprocess.run(f'bin/dcm2niix -o {fout} -f "%j_%s" -p y -z n  {file_path}', shell=True)
+    if p.returncode != 0:
+        return False
+    return True
+
+
+def get_data(path):
+    """
+    Search appopriate nii-file
+    :param path: nii-files
+    :return: file with chest and lung
+    """
+    correct_file = None
+    for file in path.glob('*.json'):
+        with pathlib.Path.open(file, 'r') as inf:
+            try:
+                data = json.load(inf)
+            except Exception as err:
+                continue
+            if 'chest' in data.get('BodyPartExamined', "").lower() and 'lung' in data.get('SeriesDescription', "").lower():
+                correct_file = file.with_suffix('.nii')
+    return correct_file
+
+
+def process_file(file_path):
+    file_path = pathlib.Path(file_path)
+    if file_path.suffix !='.nii' and \
+        file_path.suffix !='.nii.gz' and \
+        file_path.suffix != '.zip':
+        return None
+
+    guid = str(uuid.uuid4())[:8]
+    fout = pathlib.Path('downloads') / guid
+    fout.mkdir(exist_ok=True)
+
+    if file_path.suffix == '.zip': # perhaps its DICOM
+        unarchive_dir = unzip(file_path)
+        is_successful = dcm2niix(unarchive_dir, fout)
+        if not is_successful:
+            return None
+        correct_file = get_data(fout)
+
+    elif file_path.suffix =='.nii' or file_path.suffix =='.nii.gz':
+        correct_file = fout / file_path.name
+        shutil.copy(file_path, correct_file)
+
+    if correct_file:
+        mask_file = correct_file.parent / (correct_file.stem + '_mask.nii')
+        get_inference(correct_file, mask_file)
+        return [str(correct_file), str(mask_file)]
+    else:
+        return None
+
